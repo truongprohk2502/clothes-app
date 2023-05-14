9a6602d7a72d898879f0317a7735248636a4ac39
Nguyen Dinh Truong <nguyendinhtruong98@gmail.com>
2023-05-14 21:21:47 +0700

    Add nuxt link

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <header>
+      <nav>
+        <NuxtLink to="/">Nuxt Dojo</NuxtLink>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/products">Merch</NuxtLink></li>
+        </ul>
+        <a href="/about">normal link -- about page</a>
+      </nav>
+    </header>
     <h2>Home</h2>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. In perspiciatis
