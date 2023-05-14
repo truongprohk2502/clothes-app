6f5efcd35d8f435a63600ccc073f6ab4040a0001
Nguyen Dinh Truong <nguyendinhtruong98@gmail.com>
2023-05-14 21:15:50 +0700

    Add parameter route

---
+++ pages/products/[id].vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const {
+  params: { id },
+} = useRoute();
+</script>
+
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste
+      necessitatibus accusamus architecto dignissimos non harum tenetur rem
+      itaque, dolor aut?
+    </p>
+  </div>
+</template>
